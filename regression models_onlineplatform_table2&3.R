7e289e1d166e16e5a2fbea545da5c2ef5f1ccd7d
ggxxsxq <38481332+ggxxsxq@users.noreply.github.com>
2022-03-19 19:04:55 +0800

    Add files via upload

--- regression models_onlineplatform_table2&3.R
+++ regression models_onlineplatform_table2&3.R
@@ -9,21 +9,22 @@ dta$family_ses<-relevel(dta$family_ses, ref="high")
 dta$rural_urban<-relevel(dta$rural_urban, ref="urban") 
 
 ###### DV: academic improvement(table 2) #####
-## family ses
-model1<-lmRob(improvement~gender+course_before+family_ses+scale(gold_coin),data=dta)
+## city tiers
+model1<-lmRob(improvement~gender+course_before+city_tier+scale(gold_coin),data=dta)
 summary(model1)
 
 ## rural-urban
 model2<-lmRob(improvement~gender+course_before+rural_urban+scale(gold_coin),data=dta)
 summary(model2)
 
-## city tiers
-model3<-lmRob(improvement~gender+course_before+city_tier+scale(gold_coin),data=dta)
+## school status 
+model3<-lmRob(improvement~gender+course_before+school_status+scale(gold_coin),data=dta)
 summary(model3)
 
-## school status 
-model4<-lmRob(improvement~gender+course_before+school_status+scale(gold_coin),data=dta)
+## family SES
+model4<-lmRob(improvement~gender+course_before+family_ses+scale(gold_coin),data=dta)
 summary(model4)
+
 
 ## Full model
 model5<-lmRob(improvement~gender+course_before+family_ses+rural_urban+school_status+city_tier+scale(gold_coin)
@@ -32,38 +33,42 @@ summary(model5)
 
 
 ##### DV: post score (table 3)#####
-## family ses
-model6<-lmRob(scale(post_score)~gender+family_ses+scale(gold_coin)+scale(pre_score)+ course_before,data=dta)
+# city tiers
+model6<-lmRob(scale(post_score)~gender+city_tier+scale(gold_coin)+scale(pre_score)+ course_before
+               ,data=dta)
+summary(model6)
+# city tiers * golden coins
+model7<-lmRob(scale(post_score)~gender+city_tier*scale(gold_coin)+scale(pre_score)+ course_before
+               ,data=dta)
 summary(model7)
-## family ses * golden coins
-model7<-lmRob(scale(post_score)~gender+family_ses*scale(gold_coin)+scale(pre_score)
-                + course_before+scale(gold_coin),data=dta)
-summary(model7)
+
 
 ## rural-urban
 model8<-lmRob(scale(post_score)~gender+rural_urban+scale(gold_coin)+scale(pre_score)+ course_before,data=dta)
 summary(model8)
 ## rural-urban* golden coins
 model9<-lmRob(scale(post_score)~gender+rural_urban*scale(gold_coin)+scale(pre_score)
-                + course_before+scale(gold_coin),data=dta)
+              + course_before+scale(gold_coin),data=dta)
 summary(model9)
 
-# city tiers
-model10<-lmRob(scale(post_score)~gender+city_tier+scale(gold_coin)+scale(pre_score)+ course_before
-               ,data=dta)
-summary(model10)
-# city tiers * golden coins
-model11<-lmRob(scale(post_score)~gender+city_tier*scale(gold_coin)+scale(pre_score)+ course_before
-               ,data=dta)
-summary(model11)
 
 # school status
-model12<-lmRob(scale(post_score)~gender+school_status+scale(gold_coin)+scale(pre_score)+ course_before,data=dta)
-summary(model12)
+model10<-lmRob(scale(post_score)~gender+school_status+scale(gold_coin)+scale(pre_score)+ course_before,data=dta)
+summary(model10)
 # school status* golden coins
-model13<-lmRob(scale(post_score)~gender+school_status*scale(gold_coin)+scale(pre_score)
-                 + course_before+scale(gold_coin),data=dta)
+model11<-lmRob(scale(post_score)~gender+school_status*scale(gold_coin)+scale(pre_score)
+               + course_before+scale(gold_coin),data=dta)
+summary(model11)
+
+
+## family ses
+model12<-lmRob(scale(post_score)~gender+family_ses+scale(gold_coin)+scale(pre_score)+ course_before,data=dta)
+summary(model12)
+## family ses * golden coins
+model13<-lmRob(scale(post_score)~gender+family_ses*scale(gold_coin)+scale(pre_score)
+                + course_before+scale(gold_coin),data=dta)
 summary(model13)
+
 
 # Full model
 model14<-lmRob(scale(post_score)~scale(pre_score)+gender+family_ses*scale(gold_coin)+school_status*scale(gold_coin)+city_tier*scale(gold_coin)+rural_urban*scale(gold_coin)
